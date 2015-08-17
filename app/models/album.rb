b68619aa69990b708497e0a6d32335a23d6258c5
drabelpdx <drabelpdx@gmail.com>
2015-08-17 12:05:25 -0700

    more cleanup

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,15 +1,4 @@
 class Album < ActiveRecord::Base
-  @albums = @albums.paginate(:page => params[:page])
 
-  respond_to do |format|
-    format.json {
-      render :json => {
-        :current_page => @albums['pagination'].current_page,
-        :per_page => @albums['pagination'].per_page,
-        :total_pages => @albums['pagination'].pages,
-        :total_entries => @albums['pagination'].items,
-        :entries => @albums
-      }
-    }
-  end
+
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,17 +1,4 @@
 class Artist < ActiveRecord::Base
 
-  @artists = @artists.paginate(:page => params[:page])
-
-  respond_to do |format|
-    format.json {
-      render :json => {
-        :current_page => @artists['pagination'].current_page,
-        :per_page => @artists['pagination'].per_page,
-        :total_pages => @artists['pagination'].pages,
-        :total_entries => @artists['pagination'].items,
-        :entries => @artists
-      }
-    }
-  end
 
 end
